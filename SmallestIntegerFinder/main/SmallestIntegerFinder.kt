d59746d51b5b2334d802366f12c6061ed55c0025
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-06-06 18:53:29 +0100

    Update SmallestIntegerFinder.kt

--- SmallestIntegerFinder/main/SmallestIntegerFinder.kt
+++ SmallestIntegerFinder/main/SmallestIntegerFinder.kt
@@ -1,9 +1,5 @@
 class SmallestIntegerFinder {
 
-  fun findSmallestInt(nums: List<Int>): Int {
-    var num = nums.first()
-    nums.forEach { n -> if (n < num) num = n }
-    return num
-  }
+  fun findSmallestInt(nums: List<Int>): Int = nums.min()
 
 }
