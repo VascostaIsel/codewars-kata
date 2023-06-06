3f0628855d61ada94e9b6d04e1d529627ffb2c11
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-06-06 19:38:23 +0100

    Update and rename SmallestIntegerFinder/main/SmallestIntegerFinderTest.kt to SmallestIntegerFinder/SmallestIntegerFinderTest.kt

--- SmallestIntegerFinder/SmallestIntegerFinderTest.kt
+++ SmallestIntegerFinder/SmallestIntegerFinderTest.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 class SmallestIntegerFinderTest {
 
   @Test
-  fun exampleTests() {
+  fun basicTests() {
     val sif: SmallestIntegerFinder = SmallestIntegerFinder()
     assertEquals(10, sif.findSmallestInt(listOf(15, 20, 10, 17, 22, 9001)))
   }
